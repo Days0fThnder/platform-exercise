1726c9fd8ff380cce2b6cfc0420e6344c9a023e5
Jean-Leman Rusangiza <jlrusangiza@gmail.com>
2021-02-13 14:33:36 -0800

    Initial commit

---
+++ userAuthFender/src/main/java/com/jeanleman/userAuthFender/UserAuthFenderApplication.java
@@ -0,0 +1,13 @@
+package com.jeanleman.userAuthFender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthFenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthFenderApplication.class, args);
+	}
+
+}
